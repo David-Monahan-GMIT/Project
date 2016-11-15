9d5c8e24c7560f8e182b6518388bcd115f9bfcbb
David Monahan <G00230646@gmit.ie>
2016-11-15 12:21:48 +0000

    Minor updates

--- workspace/Chat Bot/src/HelloWorld.java
+++ workspace/Chat Bot/src/HelloWorld.java
@@ -2,8 +2,8 @@ import org.alicebot.ab.*;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		Bot bot = new Bot("Alice");
+		String botName = new String("Bob");
+		Bot bot = new Bot(botName);
 		Chat chatSession = new Chat(bot);
 	}
-
 }
