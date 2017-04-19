e51b6fa2001c6631835b357247dabceccf65171a
David Monahan <G00230646@gmit.ie>
2017-04-19 11:32:08 +0100

    Minor updates and changes

---
+++ program-ab-master/src/main/resources/test.java
@@ -0,0 +1,50 @@
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.browser.Browser;
+
+public class test {
+
+	protected Shell shell;
+
+	/**
+	 * Launch the application.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		try {
+			test window = new test();
+			window.open();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Open the window.
+	 */
+	public void open() {
+		Display display = Display.getDefault();
+		createContents();
+		shell.open();
+		shell.layout();
+		while (!shell.isDisposed()) {
+			if (!display.readAndDispatch()) {
+				display.sleep();
+			}
+		}
+	}
+
+	/**
+	 * Create contents of the window.
+	 */
+	protected void createContents() {
+		shell = new Shell();
+		shell.setSize(450, 300);
+		shell.setText("SWT Application");
+		
+		Browser browser = new Browser(shell, SWT.NONE);
+		browser.setBounds(50, 61, 64, 64);
+
+	}
+}
